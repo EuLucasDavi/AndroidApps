9a83a7eaf364873a315d3274c37c27bb21154587
EuLucasDavi <lucas._.davi@hotmail.com>
2024-09-13 04:15:17 -0300

    Resolvendo problema do DarkMode

--- IMCMultipleScreens/app/src/main/java/com/example/imcmultiplescreens/MainActivity.java
+++ IMCMultipleScreens/app/src/main/java/com/example/imcmultiplescreens/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
